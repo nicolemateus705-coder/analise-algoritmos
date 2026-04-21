2ce8f8fa3269755a9c3a1751c488a4796ffcd97f
nicolemateus705-coder <nicole.mateus.705@ufrn.edu.br>
2026-04-21 19:03:09 -0300

    ordenacao.h

---
+++ ordenacao.h
@@ -0,0 +1,10 @@
+#ifndef ORDENACAO_H
+#define ORDENACAO_H
+
+#include <vector>
+using namespace std;
+
+// Ordena os elementos do vetor usando o algoritmo Insertion Sort.
+void insertionSort(vector<int>& v);
+
+#endif
